95ea1186b9b60556e68e7dd29f487dc3cc4dc0fc
Xinyoung <chenrongzheng@XinyoungdeMacBook-Pro.local>
2026-04-28 18:14:21 -0700

    fix favors page syntax error

--- app/favors/page.tsx
+++ app/favors/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '../supabase'
-#just a test
 type Exchange = {
   id: string
   status: string
